d9cb16173872bfdce19f0ff9d1507385cc654e24
Ikhor03 <imamkhoiri725@gmail.com>
2023-01-27 19:54:56 +0700

    merapikan

--- js/content.js
+++ js/content.js
@@ -1,9 +1,60 @@
+// AMBIL DATA MELALUI API 
+function getData(url, cb) {
+
+    const notFoundCard = document.querySelector('.notFound-card');
+    const alertCard = document.querySelector('.alert-card');
+
+    let news = [];
+    let temp;
+    alertCard.classList.remove('hide'); // LOADING
+    fetch(url)
+        .then(res => res.json())
+        .then((data) => {
+            temp = cb(data);
+            news.push(temp)
+        })
+        // JIKA DATA TIDAK VALID
+        .catch(() => {
+            if (url === 'hide') {
+                notFoundCard.classList.add('hide');
+            } else {
+                notFoundCard.classList.remove('hide');
+            }
+            notFoundCard.textContent = 'Data yang anda Cari tidak ditemukan!'
+        })
+        .finally(() => {
+            alertCard.classList.add('hide');
+        })
+    return news;
+}
+
+// COBA FUNCTION INI DI RETURN, TERUS HASIL RETURNNYA BERUPA urlLink YANG TERUS DIJADIKAN PARAMETER UNTUK loadData;
+let flag = true;
+function searchUrl(value) {
+
+    if (value.length > 0) {
+        flag = false;
+        return `https://newsapi.org/v2/everything?q=${value}&apiKey=1ec087e86b5143d28480549839fbe11c`;
+    } else {
+        flag = true;
+        return 'https://newsapi.org/v2/top-headlines?country=id&apiKey=1ec087e86b5143d28480549839fbe11c'
+    }
+
+}
+
+
+
 function liveSearch(cb) {
 
     const notFoundCard = document.querySelector('.notFound-card');
     const searchInput = document.getElementById('searchInput');
-    const alertCard = document.querySelector('.alert-card');
-    let data = getData(cb);
+    let urlHeadline = 'https://newsapi.org/v2/top-headlines?country=id&apiKey=1ec087e86b5143d28480549839fbe11c'
+    let data = getData(urlHeadline, cb);
+
+    // NOTE : Function "searchUrl" untuk handle value input search belum ketemu
+    // searchInput.addEventListener('input', e => {
+    //    getData(searchUrl(e.target.value.toLowerCase()), cb) ;
+    // })
 
     // Live search
     let value;
@@ -18,71 +69,6 @@ function liveSearch(cb) {
             !isFound.includes(true) ? notFoundCard.classList.remove('hide') : notFoundCard.classList.add('hide') // JIKA DATA TIDAK SESUAI
         })
     })
-}
-
-// AMBIL DATA MELALUI API 
-function getData(cb) {
-
-    const searchInput = document.getElementById('searchInput');
-    const notFoundCard = document.querySelector('.notFound-card');
-    const alertCard = document.querySelector('.alert-card');
-    let urlHeadline = 'https://newsapi.org/v2/top-headlines?country=id&apiKey=1ec087e86b5143d28480549839fbe11c'
-
-    let news = [];
-    let temp;
-    let loadData = function (url) {
-        alertCard.classList.remove('hide'); // LOADING
-        fetch(url)
-            .then(res => res.json())
-            .then((data) => {
-                temp = cb(data);
-                news.push(temp)
-            })
-            // JIKA DATA TIDAK VALID
-            .catch(() => {
-                if (url === 'hide') {
-                    notFoundCard.classList.add('hide');
-                } else {
-                    notFoundCard.classList.remove('hide');
-                }
-                notFoundCard.textContent = 'Data yang anda Cari tidak ditemukan!'
-            })
-            .finally(() => {
-                alertCard.classList.add('hide');
-            })
-        return news;
-    }
-
-    let loadNewsVar = loadData(urlHeadline);
-
-    let flag = true;
-    let searchUrl = function (value) {
-
-        if (value.length > 0) {
-            flag = false;
-            urlHeadline = `https://newsapi.org/v2/everything?q=${value}&apiKey=1ec087e86b5143d28480549839fbe11c`;
-        } else {
-            flag = true;
-            urlHeadline = 'https://newsapi.org/v2/top-headlines?country=id&apiKey=1ec087e86b5143d28480549839fbe11c'
-        }
-
-        return function () {
-            if (!flag) {
-                loadNewsVar = loadData(urlHeadline);
-            } else {
-                loadNewsVar = loadData('hide');
-            }
-        }
-    }
-
-    // NOTE : Function "searchUrl" untuk handle value input search belum ketemu
-    searchInput.addEventListener('input', e => {
-        let loadNews = searchUrl(e.target.value.toLowerCase());
-        // loadNews(); 
-    })
-
-    return loadNewsVar;
-
 }
 
 export function printNews() {
